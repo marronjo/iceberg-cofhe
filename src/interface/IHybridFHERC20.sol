8b4cad5fe815c4b59268af8034d3a1b9a62cbad8
Joe Marron <marronjo@tcd.ie>
2025-04-25 15:56:49 +0100

    add hybrid fherc20 base interface

---
+++ src/interface/IHybridFHERC20.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.8.19 <0.9.0;
+
+// SPDX-License-Identifier: MIT
+// Fhenix Protocol (last updated v0.1.0) (token/FHERC20/IFHERC20.sol)
+// Inspired by OpenZeppelin (https://github.com/OpenZeppelin/openzeppelin-contracts) (token/ERC20/IERC20.sol)
+
+interface IHybridFHERC20 {
+    
+}
