0dcde7928c1773284f1c80f0339f6b377f83d1b9
Joe Marron <marronjo@tcd.ie>
2025-04-29 15:07:53 +0100

    add helper methods to queue contract

--- src/Queue.sol
+++ src/Queue.sol
@@ -1,22 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {euint128} from "@fhenixprotocol/cofhe-contracts/FHE.sol";
 import {DoubleEndedQueue} from "openzeppelin-contracts/contracts/utils/structs/DoubleEndedQueue.sol";
 
 contract Queue {
 
     DoubleEndedQueue.Bytes32Deque private queue;
 
-    function push(bytes32 handle) external {
-        DoubleEndedQueue.pushFront(queue, handle);
+    function push(euint128 handle) external {
+        DoubleEndedQueue.pushFront(queue, euintToBytes32(handle));
     }
 
-    function pop() external returns(bytes32) {
-        return DoubleEndedQueue.popFront(queue);
+    function pop() external returns(euint128) {
+        return bytes32ToEuint(DoubleEndedQueue.popFront(queue));
     }
  
-    function peek() external view returns(bytes32) {
-        return DoubleEndedQueue.front(queue);
+    function peek() external view returns(euint128) {
+        return bytes32ToEuint(DoubleEndedQueue.front(queue));
     }
 
     function length() external view returns(uint256) {
@@ -25,5 +26,13 @@ contract Queue {
 
     function isEmpty() external view returns(bool) {
         return DoubleEndedQueue.empty(queue);
+    }
+
+    function euintToBytes32(euint128 input) private pure returns(bytes32){
+        return bytes32(euint128.unwrap(input));
+    }
+
+    function bytes32ToEuint(bytes32 input) private pure returns(euint128){
+        return euint128.wrap(uint256(input));
     }
 }
