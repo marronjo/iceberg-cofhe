26a3dbb5c019053105a0ab3c41d894fa9e6ab6cb
Joe Marron <marronjo@tcd.ie>
2025-04-30 16:03:57 +0100

    inherit IERC20 interface in IFHERC20 to allow for easier integration

--- src/interface/IFHERC20.sol
+++ src/interface/IFHERC20.sol
@@ -4,9 +4,10 @@ pragma solidity >=0.8.19 <0.9.0;
 // Fhenix Protocol (last updated v0.1.0) (token/FHERC20/IFHERC20.sol)
 // Inspired by OpenZeppelin (https://github.com/OpenZeppelin/openzeppelin-contracts) (token/ERC20/IERC20.sol)
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {InEuint128, euint128} from "@fhenixprotocol/cofhe-contracts/FHE.sol";
 
-interface IFHERC20 {
+interface IFHERC20 is IERC20 {
     // -------- Public Mint / Burn Functions --------
     function mint(address user, uint256 amount) external;
     function burn(address user, uint256 amount) external;
