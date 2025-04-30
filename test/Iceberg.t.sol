dc1e53a54dddc4ef5d721f928f4cf9321eb6f8a6
Joe Marron <marronjo@tcd.ie>
2025-04-30 12:04:08 +0100

    add iceberg cofhe test, import cofhe mcok contracts

---
+++ test/Iceberg.t.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+//Foundry Imports
+import "forge-std/Test.sol";
+
+//Uniswap Imports
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {TickMath} from "v4-core/src/libraries/TickMath.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
+import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
+import {Iceberg} from "../src/Iceberg.sol";
+import {StateLibrary} from "v4-core/src/libraries/StateLibrary.sol";
+
+import {LiquidityAmounts} from "v4-core/test/utils/LiquidityAmounts.sol";
+import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
+import {EasyPosm} from "./utils/EasyPosm.sol";
+import {Fixtures} from "./utils/Fixtures.sol";
+
+//FHE Imports
+import {FHE, euint128, euint128, euint32, ebool, InEuint128, InEuint32, InEbool} from "@fhenixprotocol/cofhe-contracts/FHE.sol";
+import {CoFheTest} from "@fhenixprotocol/cofhe-foundry-mocks/CoFheTest.sol";
+
+contract IcebergTest is Test, Fixtures {
+    using EasyPosm for IPositionManager;
+    using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+    using StateLibrary for IPoolManager;
+
+    //test instance with useful utilities for testing FHE contracts locally
+    CoFheTest CFT;
+
+    Iceberg hook;
+    PoolId poolId;
+
+    uint256 tokenId;
+    int24 tickLower;
+    int24 tickUpper;
+
+    function setUp() public {
+        //initialise new CoFheTest instance with logging turned on
+        CFT = new CoFheTest(true);
+
+        // creates the pool manager, utility routers, and test tokens
+        deployFreshManagerAndRouters();
+        deployMintAndApprove2Currencies();
+
+        deployAndApprovePosm(manager);
+
+        // Deploy the hook to an address with the correct flags
+        address flags = address(
+            uint160(
+                Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.AFTER_INITIALIZE_FLAG
+            ) ^ (0x4444 << 144) // Namespace the hook to avoid collisions
+        );
+        bytes memory constructorArgs = abi.encode(manager); //Add all the necessary constructor arguments from the hook
+        deployCodeTo("Iceberg.sol:Iceberg", constructorArgs, flags);
+        hook = Iceberg(flags);
+
+        vm.label(address(hook), "hook");
+        vm.label(address(this), "test");
+
+        // Create the pool
+        key = PoolKey(currency0, currency1, 3000, 60, IHooks(hook));
+        poolId = key.toId();
+        manager.initialize(key, SQRT_PRICE_1_1);
+
+        // Provide full-range liquidity to the pool
+        tickLower = TickMath.minUsableTick(key.tickSpacing);
+        tickUpper = TickMath.maxUsableTick(key.tickSpacing);
+
+        uint128 liquidityAmount = 100e18;
+
+        (uint256 amount0Expected, uint256 amount1Expected) = LiquidityAmounts.getAmountsForLiquidity(
+            SQRT_PRICE_1_1,
+            TickMath.getSqrtPriceAtTick(tickLower),
+            TickMath.getSqrtPriceAtTick(tickUpper),
+            liquidityAmount
+        );
+
+        (tokenId,) = posm.mint(
+            key,
+            tickLower,
+            tickUpper,
+            liquidityAmount,
+            amount0Expected + 1,
+            amount1Expected + 1,
+            address(this),
+            block.timestamp,
+            ZERO_BYTES
+        );
+    }
+
+    function testPlaceIcebergOrder() public {
+        InEuint32 memory lower = CFT.createInEuint32(10, address(this));
+        InEbool memory zeroForOne = CFT.createInEbool(true, address(this));
+        InEuint128 memory liquidity = CFT.createInEuint128(100, address(this));
+
+        hook.placeIcebergOrder(key, lower, zeroForOne, liquidity);
+    }
+}
