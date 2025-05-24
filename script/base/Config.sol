e3e29b12f907b124d09db81f7d7d3f996279e680
Joe Marron <marronjo@tcd.ie>
2025-05-24 18:17:38 +0100

    add updated config for arbitrum / sepolia deployments

--- script/base/Config.sol
+++ script/base/Config.sol
@@ -1,17 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import {IFHERC20} from "../../src/interface/IFHERC20.sol";
 import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
 import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
 
 /// @notice Shared configuration between scripts
 contract Config {
     /// @dev populated with default anvil addresses
-    IERC20 constant token0 = IERC20(address(0x0165878A594ca255338adfa4d48449f69242Eb8F));
-    IERC20 constant token1 = IERC20(address(0xa513E6E4b8f2a923D98304ec87F64353C4D5C853));
-    IHooks constant hookContract = IHooks(address(0x0));
+    IFHERC20 immutable token0;
+    IFHERC20 immutable token1; 
+    IHooks immutable hookContract;
 
-    Currency constant currency0 = Currency.wrap(address(token0));
-    Currency constant currency1 = Currency.wrap(address(token1));
+    Currency immutable currency0; 
+    Currency immutable currency1;
+
+    constructor(){
+        if(block.chainid == 11155111){      // Ethereum Sepolia
+            token0 = IFHERC20(address(0x0eC274fFB635b534086716855BAc795b841BD490));
+            token1 = IFHERC20(address(0xaAA70eC4269B182fa49Cec06C9617aa38b12A647));
+            hookContract = IHooks(address(0x4402948CD6fe4fb6070DEA39B7AB9b25e5CB90C0));
+            currency0 = Currency.wrap(address(token0));
+            currency1 = Currency.wrap(address(token1));
+        } else if(block.chainid == 421614){ // Arbitrum Sepolia
+            token0 = IFHERC20(address(0x29844F233a9b5411800a7264F15CfF794A9F4303));
+            token1 = IFHERC20(address(0x3585004F86af7b95B8aD63a898C90279B101b678));
+            hookContract = IHooks(address(0xD5D3106da310E6cC784D52F46638EF3f0586d0c0));
+            currency0 = Currency.wrap(address(token0));
+            currency1 = Currency.wrap(address(token1));
+        }
+    }
 }
